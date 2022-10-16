fc000c43c096f5f1c02cf66772e45865ce98bedb
Dea Fitria <deyafitriaa@gmail.com>
2022-10-16 13:34:26 +0700

    backend

---
+++ src/java/dao/TransactionsDao.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.pojo.Transactions;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Dea Fitria
+ */
+public class TransactionsDao extends BaseDao {
+    
+    List<Transactions> listTransaction = new ArrayList<>();
+    Transactions transaction;
+    
+    public List<Transactions> getHistory(int userId) {
+        session = hibernateUtil.openSession();
+        try {
+            trans = session.beginTransaction();
+            
+            Criteria criteria = session.createCriteria(Transactions.class);
+            criteria.setFetchMode("users", FetchMode.JOIN);
+            criteria.add(Restrictions.eq("users.id", userId));
+            criteria.setFetchMode("products", FetchMode.JOIN);
+            criteria.setFetchMode("payments", FetchMode.JOIN);
+            
+            System.out.println("A");
+            listData = criteria.list();
+            System.out.println(listData);
+            trans.commit();
+        }
+        catch(HibernateException e) {
+            e.getMessage();
+        }
+        finally {
+            session.close();
+        }
+        if (!listData.isEmpty()) {
+            listData.forEach((list) -> {
+               listTransaction.add(Transactions.class.cast(list));
+            });
+        }
+        
+        return listTransaction;
+    }
+    
+    public static void main(String[] args) {
+        TransactionsDao dao = new TransactionsDao();
+        List<Transactions> lists = dao.getHistory(1);
+        
+        lists.forEach((list) -> {
+            System.out.println(list);
+            System.out.println(list.getProducts().getId());
+            System.out.println(list.getPayments().getName());
+            System.out.println(list.getName());
+        });
+    }
+}
---
+++ src/java/dao/UsersDao.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.pojo.Users;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Dea Fitria
+ */
+public class UsersDao extends BaseDao {
+    
+    Users user;
+    
+    public Users login(String email, String password) {
+        Session session = hibernateUtil.openSession();
+        Transaction trans = null;
+        try {
+            trans = session.beginTransaction();
+            
+            Query query = session.createQuery("FROM Users WHERE Email = :email AND Password = :password");
+            query.setString("email", email);
+            query.setString("password", password);
+            user = (Users)query.uniqueResult();
+        }
+        catch(HibernateException e) {
+            e.getMessage();
+        }
+        finally {
+            session.close();
+        }
+        return user;
+    }
+    
+    public Users getById(int id) {
+        return Users.class.cast(super.getById(Users.class, id));
+    }
+    
+    public static void main(String[] args) {
+        UsersDao dao = new UsersDao();
+//        Users user = dao.login("admin@gmail.com", "321");
+        Users user = dao.getById(1);
+        
+        System.out.println(user);
+        System.out.println(user.getEmail());
+        System.out.println(user.getPassword());
+        System.out.println(user.getName());
+        System.out.println(user.getAddress());
+    }
+}
